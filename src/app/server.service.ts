44eeba65df80f82017fdd553a6b9e84f638cd15b
mohamamd <mohamamdaymanayman@gmail.com>
2021-08-01 11:11:32 +0300

    first commit

---
+++ src/app/server.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@angular/core";
+import {from} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+
+Injectable()
+export class ServerService{
+  constructor(private http : HttpClient) {
+
+
+  }
+}
